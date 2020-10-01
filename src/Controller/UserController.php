92e9707173b4dd21aa44f58b6e3110d02b9b9e59
Sophie <euronda@hotmail.fr>
2020-10-01 16:10:41 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -26,8 +26,7 @@ class UserController extends ApiController
     public function addUser(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
-        
-        $dataset = ['firstName', 'lastName', 'email', 'password', 'birthday', 'role', 'location'];
+        $dataset = ['firstName', 'lastName', 'email', 'password', 'birthdate', 'role', 'location'];
 
         foreach ($dataset as $property) {
             if (empty($data[$property])) {
@@ -39,15 +38,15 @@ class UserController extends ApiController
         $lastName = $data['lastName'];
         $email = $data['email'];
         $password = $data['password'];
-        $birthday = $data['birthday'];
+        $birthdate = $data['birthdate'];
         $role = $data['role'];
         $location = $data['location'];
 
-        // if (empty($firstName) || empty($lastName) || empty($email) || empty($password) || empty($birthday) || empty($role) || empty($zip)) {
+        // if (empty($firstName) || empty($lastName) || empty($email) || empty($password) || empty($birthdate) || empty($role) || empty($zip)) {
         //     throw new NotFoundHttpException('Expecting mandatory parameters!');
         // }
 
-        if ($this->userRepository->saveUser($firstName, $lastName, $email, $password, $birthday, $role, $location)) {
+        if ($this->userRepository->saveUser($firstName, $lastName, $email, $password, $birthdate, $role, $location)) {
             return new JsonResponse(['status' => 'User created!'], Response::HTTP_CREATED);
         }
     }
