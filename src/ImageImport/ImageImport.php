b4000a53f05635139f0fbc4a317dd70d6f775cfa
Bari Jonathan <bari.jonathan@live.fr>
2020-10-14 18:13:24 +0200

    Guess mime dump

--- src/ImageImport/ImageImport.php
+++ src/ImageImport/ImageImport.php
@@ -24,7 +24,7 @@ class ImageImport
         $uniqId = $this->uniqId();
         $target_file = $target_dir . $uniqId;
         $error = false;
-        $imageFileType = mime_content_type ($_FILES[$file]['tmp_name']);
+        $imageFileType = mime_content_type ($_FILES[$file]['name']);
 
         $check = getimagesize($_FILES[$file]['tmp_name']);
         if ($check !== false) {
