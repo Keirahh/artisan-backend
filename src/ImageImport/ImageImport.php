f1bd9eff3815d75f592e9502528c4a9cb7366601
Bari Jonathan <bari.jonathan@live.fr>
2020-10-14 18:23:48 +0200

    Rollback

--- src/ImageImport/ImageImport.php
+++ src/ImageImport/ImageImport.php
@@ -2,28 +2,16 @@
 
 namespace App\ImageImport;
 
-use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ImageImport
 {
-    function uniqId($length = 13) {
-        if (function_exists("random_bytes")) {
-            $bytes = random_bytes(ceil($length / 2));
-        } elseif (function_exists("openssl_random_pseudo_bytes")) {
-            $bytes = openssl_random_pseudo_bytes(ceil($length / 2));
-        } else {
-            throw new Exception("no cryptographically secure random function available");
-        }
-        return substr(bin2hex($bytes), 0, $length);
-    }
-
     public function upload($file)
     {
         $target_dir = $_SERVER['DOCUMENT_ROOT'] . '/images/';
         $target_file = $target_dir . basename($_FILES[$file]['name']);
         $error = false;
-        $imageFileType = mime_content_type($_FILES[$file]['name']);
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
         $check = getimagesize($_FILES[$file]['tmp_name']);
         if ($check !== false) {
@@ -47,7 +35,7 @@ class ImageImport
 
         // Allow certain file formats
         if ($imageFileType != 'jpg' && $imageFileType != 'png' && $imageFileType != 'jpeg') {
-            $message = 'Sorry, only JPG, JPEG & PNG files are allowed.' . $imageFileType . ' sent';
+            $message = 'Sorry, only JPG, JPEG & PNG files are allowed.';
             $error = true;
         }
 
