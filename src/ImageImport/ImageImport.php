d1220d65f1136e2a86671346981f0bac604fdf9f
Bari Jonathan <bari.jonathan@live.fr>
2020-10-14 17:30:39 +0200

    Remove echo

--- src/ImageImport/ImageImport.php
+++ src/ImageImport/ImageImport.php
@@ -45,7 +45,6 @@ class ImageImport
             throw new \Exception($message);
         } else {
             if (move_uploaded_file($_FILES[$file]['tmp_name'], $target_file)) {
-                echo 'The file ' . htmlspecialchars(basename($_FILES[$file]['name'])) . ' has been uploaded.';
                 return $target_file;
             }
         }
