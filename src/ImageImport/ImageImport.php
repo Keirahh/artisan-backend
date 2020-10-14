2d418fdcc90a8689ffc5d1026edab9593b7743a7
Bari Jonathan <bari.jonathan@live.fr>
2020-10-14 17:00:43 +0200

    Upload path

--- src/ImageImport/ImageImport.php
+++ src/ImageImport/ImageImport.php
@@ -8,7 +8,7 @@ class ImageImport
 {
     public function upload($file)
     {
-        $target_dir = $_SERVER['DOCUMENT_ROOT'] . '/images/';
+        $target_dir = '/images/';
         $target_file = $target_dir . basename($_FILES[$file]['name']);
         $error = false;
         $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
